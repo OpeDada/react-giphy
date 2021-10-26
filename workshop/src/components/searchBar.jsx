5dc60fb151be9b9b2d7dcd66b5efd291ca791255
OpeDada <opedada@gmail.com>
2021-10-26 22:25:28 +0900

    added an event handler

--- workshop/src/components/searchBar.jsx
+++ workshop/src/components/searchBar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <input
-      type="text"
-      className="form-control
-      form-search"
-      placeholder=""
-    />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class SearchBar extends React.Component {
+  handleChange = (event) => {
+    const { changeGifIds } = this.props;
+    changeGifIds(event.currentTarget.value);
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control
+        form-search"
+        placeholder="Search"
+        onChange={this.handleChange}
+      />
+    );
+  }
 };
 
 export default SearchBar;
