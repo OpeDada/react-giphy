d7e85ffda12aeb446cd7324d7bb6e122cec7066e
OpeDada <opedada@gmail.com>
2021-10-26 21:04:32 +0900

    started with a default gif

--- workshop/src/components/gif.jsx
+++ workshop/src/components/gif.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Gif = () => {
   return (
     <div>
-
+      <img src="https://media.giphy.com/media/gG6OcTSRWaSis/giphy.gif" alt="gif" className="gif" />
     </div>
   );
 };
